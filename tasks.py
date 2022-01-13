8e0a495abd53fefd64ec0b5cb4f82183808e359a
Kacper <k.trzepiecinski@gmail.com>
2022-01-13 01:02:40 +0100

    update requirements

--- tasks.py
+++ tasks.py
@@ -2,11 +2,10 @@ import celery
 from os import getenv
 
 app = celery.Celery("scraper")
-app.conf.update(BROKER_URL=getenv('BROKER_URL'))
+app.conf.update(BROKER_URL=getenv("BROKER_URL"))
 
 
 @app.task
 def scraper(function, username, password, query, n_pages):
     results = function(username, password, query, n_pages)
     return results
-
