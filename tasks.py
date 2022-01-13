f0781ff21d8ef3ad131ef9c1a346f05e21b87610
Kacper <k.trzepiecinski@gmail.com>
2022-01-13 23:21:18 +0100

    update procfile

--- tasks.py
+++ tasks.py
@@ -1,14 +1,14 @@
-import celery
+from celery import Celery
 import os
 from app.agents.scraper import Scraper
 
-app = celery.Celery("scraper")
+app = Celery("scraper")
 app.conf.update(
     BROKER_URL=os.environ["REDIS_URL"], CELERY_RESULT_BACKEND=os.environ["REDIS_URL"]
 )
 
 
-@app.task
+@app.task()
 def scraper(username, password, query, n_pages):
     results = Scraper(username, password, query, n_pages)
     return results
