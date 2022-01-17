80b1cdb4119636c4f8d1fb7b96990de3bed9ba95
Kacper <k.trzepiecinski@gmail.com>
2022-01-18 00:11:32 +0100

    update

--- tasks.py
+++ tasks.py
@@ -2,13 +2,14 @@ from celery import Celery
 import os
 from app.agents.scraper import Scraper
 
-app = Celery("scraper")
+app = Celery(__name__)
 app.conf.update(
-    BROKER_URL=os.environ["REDIS_URL"], CELERY_RESULT_BACKEND=os.environ["REDIS_URL"]
+    BROKER_URL=os.environ["REDIS_URL"],
+    CELERY_RESULT_BACKEND=os.environ["REDIS_URL"]
 )
 
 
-@app.task()
+@app.task(name="scraper")
 def scraper(username, password, query, n_pages):
     results = Scraper(username, password, query, n_pages)
     return results
